fcfed1a17e532e459ba615e1f84c077e95c49544
Cristina Murillo <murillocam96@gmail.com>
2019-01-17 10:51:57 -0500

    added roller icon and fixed header display

--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -3,6 +3,7 @@ import {withRouter} from 'react-router-dom'
 import ResultsCont from './ResultsCont';
 import axios from 'axios'
 import hammer from '../hammer.png'
+import roller from '../roller.png'
 
 class Landing extends Component {
 
@@ -49,10 +50,10 @@ class Landing extends Component {
     render() {
         return (
             <React.Fragment>
-
                 <img id="icon1" src={hammer}/>
+                <img id="icon2" src={roller}/>
             <div>
-                <h1 id="header">Find the issues of your fave Github repo!!</h1>
+                <h1 id="header">Explore Github Issues!</h1>
                 <form onSubmit={this.submitHandler}>
                     <input type="text" name="owner" value={this.state.owner} placeholder="Repository Owner" onChange={this.changeHandler} required/>
                     <input type="text" name="repo" value={this.state.repo} placeholder="Repository Name" onChange={this.changeHandler} required/>
