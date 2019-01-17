fa69853cda63b1806faab26f0861793ed33379a1
Cristina Murillo <murillocam96@gmail.com>
2019-01-17 00:11:54 -0500

    began issue display, installed markdown component

--- src/Components/ResultsCont.js
+++ src/Components/ResultsCont.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {withRouter} from 'react-router-dom'
+import IssueView from './IssueView';
 
 class ResultsCont extends Component {
 
@@ -12,9 +13,11 @@ class ResultsCont extends Component {
                 <p>{error}</p>
             )
         } else {
+            console.log(results)
             return (
                 <div>
-                    <p>we got results</p>
+                    <h2>Project Issues:</h2>
+                    <IssueView issue={results.data[0]}/>
                 </div>
             );
     }}
